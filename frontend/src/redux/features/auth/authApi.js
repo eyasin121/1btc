2e3d8d46bf14dd88e9f1a16882757ba5ca63b060
eyasin121 <arafathossain000121@gmail.com>
2024-12-29 19:52:09 +0600

    Update authApi.js

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://btc-q3zg.onrender.com/api/user',
+        baseUrl: 'https://onebtc-lsvo.onrender.com/api/user',
         credentials: 'include',
     }),
     endpoints: (builder) => ({
